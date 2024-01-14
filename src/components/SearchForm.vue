3284e590757ec5bfba0afe4b11671df0589d9d21
edison rosales <edisonrosalescampos@gmail.com>
2024-01-14 18:30:34 -0400

    actualizando placeholder

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -15,7 +15,7 @@ const submitForm = () => {
     <div class="row justify-content-center">
       <div class="col-md-8">
         <div class="search-container">
-          <input type="text" class="form-control" placeholder="Type a search..." maxlength="100" v-model="txtSearchTerm">
+          <input type="text" class="form-control" placeholder="Type a search or empty for all results..." maxlength="100" v-model="txtSearchTerm">
           <button class="addon">
             <i class="fa fa-search"></i>
           </button>
